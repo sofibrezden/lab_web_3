7b395ed1bcd1a3a23f3371879168b17115f59be3
sofiaprg <sofiaskymail@gmail.com>
2023-12-04 23:34:44 +0200

    Add ShareAccessmodel

--- users/models.py
+++ users/models.py
@@ -1,14 +1,32 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
+
+
+class SharedAccess(models.Model):
+    note = models.ForeignKey('Note', on_delete=models.CASCADE)
+    shared_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='shared_access_notes')
+    last_edit = models.DateTimeField()
+
+    def update_last_edit(self):
+        self.last_edit = timezone.now()
+        self.save()
+
 
 class Note(models.Model):
     id = models.AutoField(primary_key=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=150)
-    content = models.TextField(blank=True, null=True)
+    content = models.TextField(blank=True)
     category = models.TextField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    users_with_edit_access = models.ManyToManyField(
+        User,
+        through='SharedAccess',
+        through_fields=('note', 'shared_user'),
+        related_name='edit_access_notes'
+    )
 
     class Meta:
         ordering = ('title',)
@@ -16,3 +34,8 @@ class Note(models.Model):
     def __str__(self):
         return f'{self.user.username} Note'
 
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        shared_access_list = SharedAccess.objects.filter(note=self)
+        for shared_access in shared_access_list:
+            shared_access.update_last_edit()
